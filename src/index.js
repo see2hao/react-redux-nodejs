9364bcdddfb02d0ce7c5e23a35fe255adca61488
see2hao <hulang1hao@163.com>
2018-07-07 20:07:31 +0800

    'a'

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom' 
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
+
+import reducers from './reducer'
+import './config'
+
+const store = createStore(reducers, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension?window.devToolsExtension():f=>f
+))
+
+ReactDOM.render(
+    (<Provider store={store}>
+        <BrowserRouter>
+        </BrowserRouter>
+    </Provider>),
+    document.getElementById('root')
+)
